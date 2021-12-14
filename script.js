9a88f009a52fe8d83b60e44261a1e9bf3c64a0a5
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2021-12-14 16:10:28 -0400

    Desafio: sidebar, em progresso

---
+++ script.js
@@ -0,0 +1,12 @@
+function showSidebar() {
+    const sidebar = document.querySelector('aside ul')
+    const sidebar2 = document.querySelector('aside i')
+    const sidebar3 = document.querySelector('aside')
+    const sidebar4 = document.querySelector('aside ul li')
+
+
+    sidebar.classList.toggle('active')   
+    sidebar2.classList.toggle('active')   
+    sidebar3.classList.toggle('active')   
+    sidebar4.classList.toggle('active')   
+}
